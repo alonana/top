ee0de38584a95a9b8287cf14cd59b0ef0b69bdf4
Alon Tamir <alonta@radware.com>
2019-01-11 08:15:02 +0200

    test push

--- python/BalancingTrees.py
+++ python/BalancingTrees.py
@@ -14,22 +14,26 @@ def min_cost(p, w):
     return 1
 
 
-print('Starting')
+def test_solution():
+    print('Starting')
 
-file_name = os.path.basename(__file__).replace(".py", ".txt")
-with open(file_name) as f:
-    for line in f.readlines():
-        if len(line.strip()) > 0:
-            print("check line {}".format(line))
-            pattern = re.compile('.*{(.*)}, {(.*)}\\s+(\\S+).*')
-            match = pattern.match(line)
-            p = get_ints(match.group(1))
-            w = get_ints(match.group(2))
-            expected = float(match.group(3))
-            actual = min_cost(p, w)
-            if actual != expected:
-                sys.stdout.flush()
-                sys.stderr.flush()
-                raise Exception("Expected: {}, but actual is {}".format(expected, actual))
+    file_name = os.path.basename(__file__).replace(".py", ".txt")
+    with open(file_name) as f:
+        for line in f.readlines():
+            if len(line.strip()) > 0:
+                print("check line {}".format(line))
+                pattern = re.compile('.*{(.*)}, {(.*)}\\s+(\\S+).*')
+                match = pattern.match(line)
+                p = get_ints(match.group(1))
+                w = get_ints(match.group(2))
+                expected = float(match.group(3))
+                actual = min_cost(p, w)
+                if actual != expected:
+                    sys.stdout.flush()
+                    sys.stderr.flush()
+                    raise Exception("Expected: {}, but actual is {}".format(expected, actual))
 
-print('Done')
+    print('Done')
+
+
+test_solution()
